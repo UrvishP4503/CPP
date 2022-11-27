4f63f4d1d1938ca7ca262123aa047b0f7f2c8d0e
Urvish <urvishp45003@gmail.com>
2022-11-27 16:53:39 +0530

    Movies list

---
+++ oop/movie/header/movie.h
@@ -0,0 +1,37 @@
+#ifndef _MOVIE_H_
+#define _MOVIE_H_
+
+#include <string>
+
+class Movie
+{ 
+private:
+    std::string name;
+    std::string rating;
+    int watched;
+
+public:
+    Movie(std::string name, std::string rating, int watched);
+    Movie(const Movie &source);
+
+    ~Movie();
+
+    //* Basic getters and setters for private attributes
+    //* implement these inline and watch your const-correctness
+    void set_name(std::string name) { this->name = name; }
+    std::string get_name() const { return name; }
+
+    void set_rating(std::string rating) { this->rating = rating; }
+    std::string get_rating() const { return rating; }
+
+    void set_watched(int watched) { this->watched = watched; }
+    int get_watched() const { return watched; }
+
+    //* Simply increment the watched attribute by 1
+    void increment_watched() { ++watched; }
+
+    //* simply displays the movie information ex.) Big, PG-13, 2
+    void display() const;
+};
+
+#endif
---
+++ oop/movie/main.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include "header/movies.h"
+
+void increment_watched(Movies &movies, std::string name);
+void add_movie(Movies &movies, std::string name, std::string rating, int watched);
+
+void increment_watched(Movies &movies, std::string name)
+{
+    if (movies.increment_watched(name))
+    {
+        std::cout << name << " watch incremented" << std::endl;
+    }
+    else
+    {
+        std::cout << name << " not found" << std::endl;
+    }
+}
+
+void add_movie(Movies &movies, std::string name, std::string rating, int watched)
+{
+    if (movies.add_movie(name, rating, watched))
+    {
+        std::cout << name << " added" << std::endl;
+    }
+    else
+    {
+        std::cout << name << " already exists" << std::endl;
+    }
+}
+
+int main()
+{
+
+    Movies my_movies;
+
+    my_movies.display();
+
+    add_movie(my_movies, "Lord of the rings", "PG-13", 20);
+    add_movie(my_movies, "Star Wars", "PG", 5);
+    add_movie(my_movies, "End Game", "PG-13", 7);
+    add_movie(my_movies, "Gangs of wasseypur","R",10);
+
+    my_movies.display();
+
+    add_movie(my_movies, "Gangs of wasseypur", "R", 10);
+    add_movie(my_movies, "Ice Age", "PG", 12);
+
+    my_movies.display();
+
+    increment_watched(my_movies, "End Game");
+    increment_watched(my_movies, "Ice Age");
+
+    my_movies.display();
+
+    increment_watched(my_movies, "Iron man");
+
+    return 0;
+}
