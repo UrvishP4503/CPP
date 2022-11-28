6b51c92a3fc61c9383b1443bbb3b629afe600958
Urvish <urvishp45003@gmail.com>
2022-11-28 22:22:47 +0530

    :D

--- array/v.cpp
+++ array/v.cpp
@@ -4,7 +4,7 @@
 int removeDuplicates(std::vector<int> &nums)
 {
     int k{1};
-    for (auto i = 1; i < nums.size(); ++i)
+    for (size_t i = 1; i < nums.size(); ++i)
         if (nums[i] != nums[i - 1])
             nums[k++] = nums[i];
     return k;
